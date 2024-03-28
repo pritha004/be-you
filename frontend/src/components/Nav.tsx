84f71b8438d5d956bd05a493fb70261c4bc5225e
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-03-28 12:25:55 +0000

    Added hamburger menu for smaller devices.

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { navLinks } from "../constants";
 import { CiWheat } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <header className="px-4 py-6 z-10 w-full text-white bg-black/50 fixed">
       <nav className="flex justify-between items-center max-container">
@@ -15,15 +23,30 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href="item.href"
-                className="font-montserrat leading-normal text-lg text-slate-gray"
+                className="font-montserrat leading-normal text-lg"
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
-          <GiHamburgerMenu />
+        <div className="hidden max-lg:flex max-lg:justify-around max-lg:px-4 max-lg:gap-4">
+          {isOpen && (
+            <ul className="flex flex-col gap-y-2 px-2 text-right">
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a
+                    href="item.href"
+                    className="font-montserrat leading-normal text-lg"
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+
+          {isOpen?<RxCross2 className="mt-1" onClick={handleMenu}/>:<GiHamburgerMenu onClick={handleMenu} />}
         </div>
       </nav>
     </header>
