c12f86659071dbbbc2cc8fa5cd945e531dc8925c
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-03-28 11:40:45 +0000

    Added color pallete and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'chocolate-brown': "#867070",
+        "skin": "#D5B4B4",
+        "pale-skin": "#E4D0D0",
+      },
+    },
   },
   plugins: [],
 }
