db98c00464810b268f9a9991ea5f304eb886a82d
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-04-02 14:07:02 +0000

    Added our values section template

--- frontend/src/sections/OurValues.tsx
+++ frontend/src/sections/OurValues.tsx
@@ -1,7 +1,25 @@
+import { BsTransparency } from "react-icons/bs";
+import { GiTransparentTubes } from "react-icons/gi";
+import { FaBottleDroplet } from "react-icons/fa6";
+
 const OurValues = () => {
   return (
-    <div>OurValues</div>
-  )
-}
+    <div>
+      OurValues 
+      Transparency
+      <BsTransparency />
+      No hidden ingredients, no opacity. Full disclosure of ingredients used &
+      their concentration
+      
+      Efficacy
+      <GiTransparentTubes />
+      Each product is launched on back of proven science only 
+      
+      Affordable
+      <FaBottleDroplet />
+      Skincare, accessible to all
+    </div>
+  );
+};
 
-export default OurValues
+export default OurValues;
