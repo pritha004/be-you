5554149ef9407e5e3dfbaab5f5200490aa9687ed
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-04-05 14:53:52 +0000

    Removed commented code and added id

--- frontend/src/sections/ProjectNature.tsx
+++ frontend/src/sections/ProjectNature.tsx
@@ -3,35 +3,7 @@ import { Button } from "../components";
 
 const ProjectNature = () => {
   return (
-    // <section id="projectNature" className="max-container">
-    //   <div className="flex gap-5 mb-4">
-    //     <div className="flex flex-col">
-    //       <h2 className="text-4xl font-bold font-palanquin">
-    //         <span className=" text-chocolate-brown">BE U</span> |{" "}
-    //         <span className=" text-chocolate-brown">Project</span>{" "}
-    //         <span className="text-skin">Nature</span>
-    //       </h2>
-
-    //     </div>
-    //   </div>
-    //   <div className="flex justify-center ">
-    //     <div className=" rounded-lg shadow-lg shadow-chocolate-brown">
-    //       <img
-    //         src={projectNature}
-    //         alt="trees"
-    //         className="rounded object-contain"
-    //         height="30%"
-    //       />
-    //     </div>
-    //     <p className="text-xl text-center">
-    //         Every time you purchase a{" "}
-    //         <span className="font-bold font-palanquin">BE U</span> , you plant a
-    //         tree.
-    //       </p>
-    //   </div>
-    // </section>
-
-    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+    <section id="projectNature" className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
       <div className="flex flex-1 justify-center items-center">
         <img
           src={projectNature}
