c9dd03ad34a2f5f2f80c7254475966126034b8d4
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-04-19 13:01:51 +0000

    Modified contact us form

--- frontend/src/pages/ContactUs.tsx
+++ frontend/src/pages/ContactUs.tsx
@@ -1,16 +1,56 @@
+import { Button } from "../components";
+
 const ContactUs = () => {
   return (
-    <section className=" sm:px-16 px-8 sm:py-24 py-28">
-      <section id="newProducts" className="max-container">
+    <section className="bg-pale-skin/30 sm:px-16 px-8 sm:py-24 py-28">
+      <section id="contactUs" className="flex max-container gap-x-2 justify-around">
         <div className="flex flex-col gap-5 mb-4">
           <h2 className="text-4xl font-bold font-palanquin capitalize">
-            Get In <span className="text-skin">Touch</span>
+            Get In <span className="text-chocolate-brown">Touch</span>
           </h2>
-          <p className="text-md font-semibold">Our friendly team would love to hear from you!</p>
+          <p className="text-xl font-semibold">
+            Our friendly team would love to hear from you!
+          </p>
           <div>
-            <form>
+            <form className="flex flex-col gap-2 text-md">
+              <div className="flex items-center gap-2 ">
+                <label className="inline-block mb-1 pl-1 w-28">Name</label>
+                <input
+                  type="text"
+                  className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-chocolate-brown`}
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="inline-block mb-1 pl-1 w-28">Email</label>
+                <input
+                  type="email"
+                  className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-chocolate-brown`}
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <label className="inline-block mb-1 pl-1 w-28">Phone number</label>
+                <input
+                  type="text"
+                  className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-chocolate-brown`}
+                />
+              </div>
+              <div className="flex items-start gap-2">
+                <label className="inline-block mb-1 pl-1 w-28">Message</label>
+                <textarea
+                  name="Text1"
+                  cols={40}
+                  rows={5}
+                  className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-chocolate-brown`}
+                ></textarea>
+              </div>
+              <div className="flex flex-wrap gap-2 mt-8">
+                <Button label="Send Message" />
+              </div>
             </form>
           </div>
+        </div>
+        <div className="flex items-center">
+            <img className="h-[450px]" src="https://images.unsplash.com/photo-1631730486784-5456119f69ae?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2tpbmNhcmUlMjBwcm9kdWN0c3xlbnwwfDF8MHx8fDI%3D"/>
         </div>
       </section>
     </section>
