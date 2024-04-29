77246db16401017a0290dc9d70571e8a92feafed
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-04-29 09:20:54 +0000

    Added link to navigate to all products.

--- frontend/src/sections/MinimalMakeup.tsx
+++ frontend/src/sections/MinimalMakeup.tsx
@@ -1,19 +1,28 @@
 import { ProductCard } from "../components";
 import { minimalMakeupProducts } from "../constants";
+import { Link } from "react-router-dom";
 
 const MinimalMakeup = () => {
-    return (
-      <section id="newProducts" className="max-container">
-      <div className="flex flex-col justify-center gap-5 mb-4">
-        <h2 className="text-4xl font-bold font-palanquin"> Discover <span className="text-skin">Your</span> Essence</h2>
+  return (
+    <section id="newProducts" className="max-container">
+      <div className="flex flex-row justify-between items-end gap-5 mb-4">
+        <h2 className="text-4xl font-bold font-palanquin">
+          Discover <span className="text-skin">Your</span> Essence
+        </h2>
+        <Link
+          to="/products"
+          className=" text-amber-900 font-semibold hover:text-chocolate-brown hover:underline"
+        >
+          View All
+        </Link>
       </div>
       <div className="grid justify-around lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
-      {minimalMakeupProducts.map((product)=>(
-        <ProductCard key={product.name} {...product}/>
-      ))}
+        {minimalMakeupProducts.map((product) => (
+          <ProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
-    )
-  }
-  
-  export default MinimalMakeup
+  );
+};
+
+export default MinimalMakeup;
--- frontend/src/sections/NewLaunches.tsx
+++ frontend/src/sections/NewLaunches.tsx
@@ -1,19 +1,28 @@
+import { Link } from "react-router-dom";
 import { ProductCard } from "../components";
 import { newLaunchProducts } from "../constants";
 
 const NewLaunches = () => {
   return (
     <section id="newProducts" className="max-container">
-    <div className="flex flex-col justify-center gap-5 mb-4">
-      <h2 className="text-4xl font-bold font-palanquin"> <span className="text-skin">New</span> Launches</h2>
-    </div>
-    <div className="grid justify-around lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
-      {newLaunchProducts.map((product)=>(
-        <ProductCard key={product.name} {...product}/>
-      ))}
-    </div>
-  </section>
-  )
-}
+      <div className="flex flex-row justify-between items-end gap-5 mb-4">
+        <h2 className="text-4xl font-bold font-palanquin">
+          <span className="text-skin">New</span> Launches
+        </h2>
+        <Link
+          to="/products"
+          className=" text-amber-900 font-semibold hover:text-chocolate-brown hover:underline"
+        >
+          View All
+        </Link>
+      </div>
+      <div className="grid justify-around lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {newLaunchProducts.map((product) => (
+          <ProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default NewLaunches
+export default NewLaunches;
