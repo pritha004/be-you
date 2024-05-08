b96cceac072a80f11676e562ea418e3b88a90d32
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-05-08 10:20:32 +0000

    Modified single product page layout.

--- frontend/src/pages/SingleProduct.tsx
+++ frontend/src/pages/SingleProduct.tsx
@@ -2,6 +2,10 @@ import { useParams } from "react-router-dom";
 import { allProducts } from "../constants";
 import { useEffect, useState } from "react";
 import { Product } from "../models/Product";
+import { MdCurrencyRupee } from "react-icons/md";
+import { IoIosAddCircle } from "react-icons/io";
+import { IoIosRemoveCircle } from "react-icons/io";
+import { Button } from "../components";
 
 const SingleProduct = () => {
   const { id } = useParams();
@@ -17,11 +21,29 @@ const SingleProduct = () => {
     <section className=" sm:px-16 px-8 sm:py-24 py-28 dark:text-white">
       <section id="product" className="max-container">
         <div className="mb-4">
-          <div className="grid grid-cols-2 gap-4 my-4">
-            <img src={product?.imgURL} />
+          <div className="grid grid-cols-2 gap-12 my-4 max-xl:grid-cols-1">
+            <img src={product?.imgURL} className="rounded" />
             <div className="">
-              <h2 className="text-justify font-bold capitalize text-4xl">{product?.name}</h2>
-              <p>{product?.desc}</p>
+              <h2 className="text-justify font-bold capitalize text-4xl my-6 text-skin">
+                {product?.name}
+              </h2>
+              <p className="my-4">{product?.desc}</p>
+              <p className="my-4 flex items-center text-xl font-semibold text-center font-montserrat leading-normal ">
+                MRP: &nbsp;
+                <MdCurrencyRupee />
+                {product?.price}
+              </p>
+              <p className="my-4 flex items-center font-semibold text-center font-montserrat leading-normal ">
+                Quantity: &nbsp;&nbsp;
+                <span className="flex items-center gap-6">
+                  <IoIosRemoveCircle fontSize={30} />
+                  1
+                  <IoIosAddCircle fontSize={30} />
+                </span>
+              </p>
+              <div className="flex flex-wrap gap-2 mt-8 w-full">
+                <Button label="ADD TO CART" borderColor="border-chocolate-brown"></Button>
+              </div>
             </div>
           </div>
         </div>
