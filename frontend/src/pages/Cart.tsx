336cb866d95511a24b35c16dcfbc400df53fb53e
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-05-21 11:57:37 +0000

    Added total section in cart

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,6 +1,8 @@
 import { CartCard } from "../components";
 import { BsFillBagHeartFill } from "react-icons/bs";
+import { MdCurrencyRupee } from "react-icons/md";
 import useCart from "../contexts/cart";
+import { CartProduct } from "../models/Product";
 
 const Cart = () => {
   const { cart } = useCart();
@@ -18,13 +20,28 @@ const Cart = () => {
             {cart.map((product) => (
               <CartCard key={product.id} {...product} />
             ))}
+            <div className="md:border my-2"></div>
+            <div className="grid md:grid-cols-12 w-full max-md:shadow-md max-md:p-2 max-md:rounded-md max-md:border max-sm:w-full dark:text-white max-md:bg-white max-md:dark:bg-[#0c1324] max-md:dark:border-black">
+              <div className="flex col-span-full md:col-span-8"></div>
+              <div className="px-4 flex col-span-full md:col-span-4 justify-between items-center md:justify-center gap-x-4 md:gap-x-10">
+                <span className="text-xl font-semibold">Total Amount:</span>
+                <p className="font-montserrat leading-normal flex items-center text-xl font-semibold">
+                  <MdCurrencyRupee />
+                  {cart.reduce(function (a, b: CartProduct) {
+                    return a + b.price * b.quantity;
+                  }, 0)}
+                </p>
+              </div>
+            </div>
           </div>
         ) : (
           <div className="pt-4 flex flex-col items-center justify-center gap-4">
-            <BsFillBagHeartFill fontSize={200} className="text-rose-400/80"/>
+            <BsFillBagHeartFill fontSize={200} className="text-rose-400/80" />
             <div className="flex flex-col justify-center items-center">
               <p className="font-bold text-lg">Hey, it feels so light!</p>
-              <p className="text-center italic dark:text-slate-400">There is nothing in your bag. Let's add some items.</p>
+              <p className="text-center italic dark:text-slate-400">
+                There is nothing in your bag. Let's add some items.
+              </p>
             </div>
           </div>
         )}
